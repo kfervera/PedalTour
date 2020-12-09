b98b892d5a1db6984fa8da2ddd11145f4b79c137
Kevin Fernández <kevin.fernandez@bit2bitamericas.com>
2020-12-08 19:14:08 -0500

    mejoras

--- app/js/services/components.js
+++ app/js/services/components.js
@@ -105,7 +105,8 @@ angular.module('pedalApp')
             $('.time').datetimepicker({
                 datepicker: false,
                 allowTimes: [
-                    '12:00', '13:00', '15:00'
+                    '06:00', '07:00', '08:00','09:00', '10:00', '11:00','12:00', '13:00',
+                    '15:00', '16:00', '17:00','18:00', '19:00', '20:00','21:00'
                 ],
                 format: 'H:i',
                 onChangeDateTime: function (dp, $input) {
